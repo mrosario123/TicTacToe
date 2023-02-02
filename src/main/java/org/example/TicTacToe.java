43b6a7441643f22279f622f3f6bb298a8dd917e0
mrosario123 <mrosario1@ggc.edu>
2023-02-01 20:34:45 -0500

    Added main function of the class

--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -4,5 +4,38 @@ import java.util.Scanner;
 public class TicTacToe {
     public static void main(String[] args){
 
+        char[][] board = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
+
+        char currentPlayer = 'X';
+        boolean gameOver = false;
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(!gameOver){
+            System.out.println("Player " + currentPlayer + ", enter row (0-2):");
+            int row = scanner.nextInt();
+            System.out.println("Player " + currentPlayer + ", enter column (0-2):");
+            int column = scanner.nextInt();
+
+            if(board[row][column] == ' '){
+                board[row][column] = currentPlayer;
+
+                if(checkWin(board, currentPlayer)){
+                    printBoard(board);
+                    System.out.println("Player " + currentPlayer + "wins");
+                    gameOver = true;
+                }else{
+
+                    if(currentPlayer == 'X'){
+                        currentPlayer = 'O';
+                    }else {
+                        currentPlayer = 'X';
+                    }
+                }
+            }else{
+                System.out.println("That spot is already taken. Please try again.");
+            }
+        }
     }
+
 }
